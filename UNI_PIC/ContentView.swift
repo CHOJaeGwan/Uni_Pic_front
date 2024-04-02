e9a88a580197966c1b52e01c693d6439fbf1d53e
2021147521 <jjgs1235@yonsei.ac.kr>
2024-04-02 17:53:19 +0900

    Initial Commit

---
+++ UNI_PIC/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UNI_PIC
+//
+//  Created by 조재관 on 2024/04/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UNI_PIC/UNI_PICApp.swift
@@ -0,0 +1,17 @@
+//
+//  UNI_PICApp.swift
+//  UNI_PIC
+//
+//  Created by 조재관 on 2024/04/02.
+//
+
+import SwiftUI
+
+@main
+struct UNI_PICApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
