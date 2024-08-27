d33deca7df1d738925e7da64590ac7cb363ae85d
CHOJaeGwan <jjgs0127@gmail.com>
2024-08-27 14:39:40 +0900

    Add signout

---
+++ UNI_PIC/View/MyAlbumView/CustomActionSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomActionSheetView.swift
+//  UNI_PIC
+//
+//  Created by 조재관 on 8/27/24.
+//
+
+import SwiftUI
+
+struct CustomActionSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomActionSheetView()
+}
